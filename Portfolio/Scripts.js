57dbca461e0b25fcbb5d581eb7262a143587db73
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-08-11 20:22:39 -0300

    adicionando o icone

--- Portfolio/Scripts.js
+++ Portfolio/Scripts.js
@@ -1,10 +1,7 @@
 import * as THREE from "https://unpkg.com/three/build/three.module.js";
 
 console.log("Scripts.js ok");
-document.addEventListener('DOMContentLoaded', function() {
-    document.body.style.overflow = "hidden";
-    document.body.style.margin = "0";
-});
+document.body.style.cssText = 'overflow: hidden; margin: 0; padding: 0';
 
 const cena = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
@@ -12,12 +9,15 @@ const renderizador = new THREE.WebGLRenderer();
 
 renderizador.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderizador.domElement);
+camera.position.z = 5;
 
-
-const material = new THREE.MeshBasicMaterial({color: 0x808080});
+const material = new THREE.MeshBasicMaterial({color: 0x708188});
 const cubo = new THREE.Mesh(new THREE.BoxGeometry(), material);
 cena.add(cubo);
 
+/**
+ * Atualiza a tela.
+ */
 function animate(){
     requestAnimationFrame(animate);
     cubo.rotation.x += 0.01;
@@ -26,4 +26,3 @@ function animate(){
 }
 animate();
 
-camera.position.z = 5;
