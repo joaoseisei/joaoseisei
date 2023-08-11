7b2d2710b061320ba9cec170327ef0edc1011496
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-08-11 20:00:52 -0300

    retirando o css

--- Portfolio/Scripts.js
+++ Portfolio/Scripts.js
@@ -1,8 +1,29 @@
-const p = document.getElementById("btn");
-p.addEventListener("click", ()=>{
-    console.log("iniciando o portifolio");
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+
+console.log("Scripts.js ok");
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.style.overflow = "hidden";
+    document.body.style.margin = "0";
 });
 
-function teste(){
+const cena = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100);
+const renderizador = new THREE.WebGLRenderer();
 
+renderizador.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderizador.domElement);
+
+
+const material = new THREE.MeshBasicMaterial({color: 0x808080});
+const cubo = new THREE.Mesh(new THREE.BoxGeometry(), material);
+cena.add(cubo);
+
+function animate(){
+    requestAnimationFrame(animate);
+    cubo.rotation.x += 0.01;
+    cubo.rotation.y += 0.01;
+    renderizador.render(cena, camera);
 }
+animate();
+
+camera.position.z = 5;
