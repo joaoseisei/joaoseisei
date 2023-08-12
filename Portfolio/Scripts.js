25933b02513695c2736cbe4844013cc86cc6d4c6
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-08-12 16:55:08 -0300

    adicionando clique do mouse

--- Portfolio/Scripts.js
+++ Portfolio/Scripts.js
@@ -17,7 +17,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHei
 camera.position.set( 0, 3, 8);
 camera.lookAt(0,0,0);
 //--------------------------------LUZ---------------------------------
-const luz = new THREE.PointLight(0xffffff, 30, 100 );
+const luz = new THREE.PointLight(0xB0C4DE, 30, 100 );
 luz.position.set(5, 5 , 0);
 luz.castShadow = true;
 cena.add(luz);
@@ -39,6 +39,20 @@ const geometriaCubo = new THREE.BoxGeometry(2, 2, 2)
 const cubo = new THREE.Mesh(geometriaCubo, cor);
 cubo.castShadow = true;
 cena.add(cubo);
+function onClick(){
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersects = raycaster.intersectObject(cubo);
+    if (intersects.length > 0) {
+        console.log("caixa abrindo");
+    }
+};
+
+renderizador.domElement.addEventListener("click", onClick);
 
 const corChao = new THREE.MeshStandardMaterial({color : 0xffffff})
 const chao = new THREE.Mesh(new THREE.PlaneGeometry(40, 40), corChao);
@@ -51,11 +65,10 @@ cena.add(chao);
 function ativarLuz(){
     const helper = new THREE.CameraHelper( luz.shadow.camera );
     cena.add( helper );
-}
-ativarLuz()
+}ativarLuz()
 
 function girarOBJ(obj){
-    obj.rotation.x = obj.rotation.y += 0.01;
+    obj.rotation.y += 0.01;
 }
 
 function animate(){
