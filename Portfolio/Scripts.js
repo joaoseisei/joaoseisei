28ddb8ae2b3352326be4979a9bf12d9dba94c380
joaoseisei <109870486+joaoseisei@users.noreply.github.com>
2023-08-14 13:31:03 -0300

    adicionando sombra e simplificando o codigo

--- Portfolio/Scripts.js
+++ Portfolio/Scripts.js
@@ -1,10 +1,12 @@
-import * as THREE from "three";
-import {GLTFLoader} from "three/loaders/GLTFLoader";
+import * as THREE from "https://unpkg.com/three@0.155.0/build/three.module.js";
+import {GLTFLoader} from "https://unpkg.com/three@0.155.0/examples/jsm/loaders/GLTFLoader.js";
 
 console.log("Scripts.js ok");
 document.body.style.cssText = 'overflow: hidden; margin: 0; padding: 0';
 
 //--------------------------------CENA---------------------------------
+const GLTF = new GLTFLoader();
+
 const cena = new THREE.Scene();
 
 const renderizador = new THREE.WebGLRenderer();
@@ -16,32 +18,15 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHei
 camera.position.set( 0, 3, 8);
 camera.lookAt(0,0,0);
 //--------------------------------LUZ---------------------------------
-const luz = new THREE.PointLight(0xB0C4DE, 30, 100 );
+const luz = new THREE.PointLight(0xB0C4DE, 35, 100 );
 luz.position.set(5, 5 , 0);
 luz.castShadow = true;
 cena.add(luz);
-//-------------------------------OBJETOS------------------------------
-const points = [];
-points.push( new THREE.Vector3( - 3, 0, 0 ) );
-points.push( new THREE.Vector3( 3, 0, 0 ) );
-const line = new THREE.Line(
-    new THREE.BufferGeometry().setFromPoints(points),
-    new THREE.LineBasicMaterial({color: 0x000000})
-);
-line.castShadow = true;
-cena.add(line);
-
-const textureURL = "https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif";
-const texture = new THREE.TextureLoader().load(textureURL);
-texture.colorSpace = THREE.SRGBColorSpace;
-const cubo = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 2, 2),
-    new THREE.MeshStandardMaterial({map: texture})
-);
-cubo.castShadow = true;
-//cena.add(cubo);
-
-
+const luz2 = new THREE.PointLight(0xB0C4DE, 35, 100 );
+luz2.position.set(-5, 5 , 0);
+luz2.castShadow = true;
+cena.add(luz2);
+//-------------------------------CHAO---------------------------------
 const chao = new THREE.Mesh(
     new THREE.PlaneGeometry(40, 40),
     new THREE.MeshStandardMaterial({color : 0xffffff})
@@ -50,20 +35,45 @@ chao.position.set(0,-2, 0);
 chao.rotation.x = -Math.PI / 2;
 chao.receiveShadow = true;
 cena.add(chao);
+//-------------------------------OBJETOS------------------------------
+const textureURL = "https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif";
+const texture = new THREE.TextureLoader().load(textureURL);
+texture.colorSpace = THREE.SRGBColorSpace;
+const cubo = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 2, 2),
+    new THREE.MeshStandardMaterial({map: texture})
+);
+cubo.position.set(8,-1,-6)
+cubo.castShadow = true;
+cena.add(cubo);
 
+let peixeGLTF;
+GLTF.load("resources/Models/scene.gltf",(gltf)=> {
+    peixeGLTF = gltf.scene;
+    peixeGLTF.scale.set(0.5,0.5,0.5);
+    peixeGLTF.position.set(0,-1,0)
+    peixeGLTF.rotation.y = Math.PI / 2;
+    sombrearModelo(peixeGLTF);
+    cena.add(peixeGLTF);
+});
+//-------------------------------FUNÇÕES------------------------------
+/**
+ * Gira um objeto.
+ * @param obj Objeto a ser girado.
+ */
 function girarOBJ(obj){
-    obj.rotation.y += 0.01;
+    if(obj!=null) obj.rotation.y += 0.01;
 }
 
-const loader = new GLTFLoader();
-loader.load("resources/Models/scene.gltf",(gltf)=> {
-    const gltfModel = gltf.scene;
-    gltfModel.scale.set(0.5,0.5,0.5);
-    gltfModel.position.set(0,0,0)
-    gltfModel.rotation.y = Math.PI / 2;
-    gltfModel.castShadow = true;
-    cena.add(gltfModel);
-});
+/**
+ * Percorre todos os filhos de um obj e sombreia eles.
+ * @param obj Objeto a ser sombreado.
+ */
+function sombrearModelo(obj){
+    obj.traverse(child => {
+        if(child.isMesh) child.receiveShadow = child.castShadow = true;
+    });
+}
 
 /**
  * Mostra o direcionamento dos raios de luz.
@@ -72,7 +82,7 @@ loader.load("resources/Models/scene.gltf",(gltf)=> {
 function ativarLuz(objLuz){
     const linhasGuia = new THREE.CameraHelper(objLuz.shadow.camera);
     cena.add(linhasGuia);
-}ativarLuz(luz)
+}
 
 /**
  * Atualiza as proporções da tela.
@@ -90,8 +100,5 @@ window.addEventListener("resize", atualizarProporcao);
 function animate(){
     requestAnimationFrame(animate);
     renderizador.render(cena, camera);
-
-    girarOBJ(line)
-    girarOBJ(cubo);
+    girarOBJ(peixeGLTF);
 }animate();
-
